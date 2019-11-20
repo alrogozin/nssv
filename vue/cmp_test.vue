a7aa37921884f0a099e36496ebef804b9a4f7eb6
alrogozin <alrogozin@gmail.com>
2019-11-20 12:33:04 +0300

    The first commit

---
+++ vue/cmp_test.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="thefirst">
+        {{msg}}
+    </div>
+</template>
+<script>
+export default {
+    name: 'thefirst',
+    data() {
+        return {
+            msg: 'welcome to ...'
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
